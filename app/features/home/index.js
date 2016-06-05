6f739a1ab4a8eb20dc78ff2c03fd6e105e30c2fa
almoanna <anna.duda181@gmail.com>
2016-06-05 12:26:07 +0000

    dyrektywa user to do i all

--- app/features/home/index.js
+++ app/features/home/index.js
@@ -3,9 +3,15 @@ import uirouter from 'angular-ui-router';
 import routing from './home.routes';
 import HomeController from './home.controller';
 import userToDo from './directives/user';
+import toDouser from './directives/user_toDo';
+//import all_ToDo from './directives/all_toDo';
+
+
 
 export default angular.module('app.home', [uirouter])
     .config(routing)
     .controller('HomeController', HomeController)
     .directive('userToDo', userToDo)
+    .directive('toDouser', toDouser)
+  //  .directive('all_ToDo', all_ToDo)
     .name;
